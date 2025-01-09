55f4e69494c554e1a64bd16485bfa93b2550f215
sgautier-dev <sgautier.dev@gmail.com>
2025-01-09 10:58:24 +0400

    hero done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,49 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+			keyframes: {
+				appear: {
+					from: { opacity: "0" },
+					to: { opacity: "1" },
+				},
+				slide: {
+					from: {
+						transform: "translateX(100%)",
+						opacity: "0",
+					},
+					to: {
+						transform: "translateX(0%)",
+						opacity: "1",
+					},
+				},
+				translate: {
+					from: {
+						transform: "translateY(20%)",
+						opacity: "0",
+					},
+					to: {
+						transform: "translateY(0)",
+						opacity: "1",
+					},
+				},
+			},
+			animation: {
+				appear: "appear 1s ease-in-out",
+				slide: "slide 750ms ease-in-out",
+				translate: "translate 2s ease-in-out forwards",
+			},
+		},
+	},
+	plugins: [],
+} satisfies Config
