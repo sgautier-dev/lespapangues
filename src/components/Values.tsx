8f9688df090b6e61d6eb295a7c80eb708fe3143b
sgautier-dev <sgautier.dev@gmail.com>
2025-01-09 17:03:45 +0400

    updated values

--- src/components/Values.tsx
+++ src/components/Values.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image"
+import heroImg from "@/images/Mains coeur_Pexels.jpg"
+
 const values = [
 	{
 		id: 1,
@@ -23,27 +26,53 @@ const values = [
 
 export default function Values() {
 	return (
-		<div className="py-24 sm:py-32">
-			<div className="mx-auto max-w-7xl px-6 lg:px-8">
-				<div className="mx-auto max-w-2xl lg:max-w-none">
-					<div className="text-center">
-						<h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">
-							Nos valeurs
-						</h2>
-					</div>
-					<dl className="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4">
-						{values.map((value) => (
-							<div key={value.id} className="flex flex-col bg-sky-400/5 p-8">
-								<dt className="text-sm/6 font-semibold text-gray-600">
-									{value.name}
-								</dt>
-								<dd className="order-first text-3xl font-semibold tracking-tight text-gray-900">
-									{value.value}
-								</dd>
-							</div>
-						))}
-					</dl>
+		<div className="relative isolate overflow-hidden bg-gray-900 mt-8 py-24 sm:py-32">
+			{/* Overlay */}
+			<div className="absolute inset-0 bg-black/50"></div>
+
+			{/* Content */}
+			<Image
+				alt="expériences aqua dance flow"
+				src={heroImg}
+				className="absolute inset-0 -z-10 size-full object-cover"
+				width={1280}
+				height={853}
+			/>
+
+			<div className="relative mx-auto max-w-7xl px-6 lg:px-8">
+				<div
+					aria-hidden="true"
+					className="absolute -bottom-8 -left-96 -z-10 transform-gpu blur-3xl sm:-bottom-64 sm:-left-40 lg:-bottom-32 lg:left-8 xl:-left-10"
+				>
+					<div
+						style={{
+							clipPath:
+								"polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+						}}
+						className="aspect-1266/975 w-[79.125rem] bg-linear-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+					/>
 				</div>
+				<div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
+					<h2 className="text-base/8 font-semibold text-sky-400">
+						Nos valeurs
+					</h2>
+					<p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-white sm:text-5xl">
+						Ouvrons ensemble la voie vers un monde de liens, de paix et d’amour
+					</p>
+				</div>
+				<dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 text-white sm:mt-20 sm:grid-cols-2 sm:gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+					{values.map((value) => (
+						<div
+							key={value.id}
+							className="flex flex-col gap-y-3 border-l border-white/10 pl-6"
+						>
+							<dt className="text-sm/6">{value.name}</dt>
+							<dd className="order-first text-3xl font-semibold tracking-tight">
+								{value.value}
+							</dd>
+						</div>
+					))}
+				</dl>
 			</div>
 		</div>
 	)
